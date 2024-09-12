1c0f2b47d706a49b172562ccd8474d78656b51cb
Mirelys Carcana Barbosa <mirelys_carcana_barbosa@brown.edu>
2024-09-12 14:15:02 -0500

    2D pre-stress debugging

--- submit_hyper.sh
+++ submit_hyper.sh
@@ -8,7 +8,10 @@
 #./mfc.sh run ./examples/3D_hyper_prestress_bubingel/case.py -p batch -N 1 -n 4 -g 0 -w 01:00:00 -# test1 -t simulation -c oscar
 #./mfc.sh run ./examples/3D_hyper_prestress_bubingel/case.py -p batch -N 1 -n 4 -g 0 -w 01:00:00 -# test1 -t post_process -c oscar
 
-./mfc.sh run ../ctr2024/jose-pc/testing/prestress/3d_gelff_prestress_3f.py -p batch -N 1 -n 4 -g 0 -w 01:00:00 -# test1 -t pre_process -c oscar
-./mfc.sh run ../ctr2024/jose-pc/testing/prestress/3d_gelff_prestress_3f.py -p batch -N 1 -n 4 -g 0 -w 01:00:00 -# test1 -t simulation -c oscar
-./mfc.sh run ../ctr2024/jose-pc/testing/prestress/3d_gelff_prestress_3f.py -p batch -N 1 -n 4 -g 0 -w 01:00:00 -# test1 -t post_process -c oscar
+#./mfc.sh run ../ctr2024/jose-pc/testing/prestress/3d_gelff_prestress_3f.py -p batch -N 1 -n 4 -g 0 -w 01:00:00 -# test1 -t pre_process -c oscar
+#./mfc.sh run ../ctr2024/jose-pc/testing/prestress/3d_gelff_prestress_3f.py -p batch -N 1 -n 4 -g 0 -w 01:00:00 -# test1 -t simulation -c oscar
+#./mfc.sh run ../ctr2024/jose-pc/testing/prestress/3d_gelff_prestress_3f.py -p batch -N 1 -n 4 -g 0 -w 01:00:00 -# test1 -t post_process -c oscar
 
+./mfc.sh run /scratch/bciv/mcarcanabarbosa/ctr2024/finalruns/elcom/afterctr/prestress/3d_gelff_prestress_3f.py -e batch -p gpuA40x4 -N 1 -n 1 -g 0 -w 01:00:00 -# test1 -t pre_process -a bciv-delta-gpu -c delta
+./mfc.sh run /scratch/bciv/mcarcanabarbosa/ctr2024/finalruns/elcom/afterctr/prestress/3d_gelff_prestress_3f.py -e batch -p gpuA40x4 -N 1 -n 1 -g 0 -w 01:00:00 -# test1 -t simulation -a bciv-delta-gpu -c delta
+./mfc.sh run /scratch/bciv/mcarcanabarbosa/ctr2024/finalruns/elcom/afterctr/prestress/3d_gelff_prestress_3f.py -e batch -p gpuA40x4 -N 1 -n 1 -g 0 -w 01:00:00 -# test1 -t post_process -a bciv-delta-gpu -c delta
