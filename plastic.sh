36119e2f2fee3f7601c6c4ee2eba7c69e0dee338
Mauro Rodriguez <mauro_rodriguez@brown.edu>
2025-01-29 17:14:15 -0500

    further cleaning to the branch

--- plastic.sh
+++
@@ -1,3 +0,0 @@
-#./mfc.sh run ./examples/2D_sucrose_plasticity/case.py -p gpuA40x4 -N 1 -n 1 -g 1 -w 01:00:00 -# test1 -t pre_process -a bciv-delta-gpu -c delta
-#./mfc.sh run ./examples/2D_sucrose_plasticity/case.py -p gpuA40x4 -N 1 -n 1 -g 1 -w 01:00:00 -# test1 -t simulation -a bciv-delta-gpu -c delta
-./mfc.sh run ./examples/2D_sucrose_plasticity/case.py -p gpuA40x4 -N 1 -n 1 -g 1 -w 01:00:00 -# test1 -t post_process -a bciv-delta-gpu -c delta
